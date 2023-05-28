5516a6a62de5b0c52d61f6e7b1f08606b51af4bd
Fauzannzz <89927233+Fauzannzz@users.noreply.github.com>
2023-05-28 21:55:42 +0700

    Update Jarkom.py

--- Jarkom.py
+++ Jarkom.py
@@ -1,48 +1,49 @@
 import socket
 
 def tcp_server():
-    SERVER_HOST = "127.0.0.1"
-    SERVER_PORT = 65000
+    SERVER_HOST = "127.0.0.1"  # Alamat IP untuk server
+    SERVER_PORT = 6500  # Port untuk server
 
-    sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Membuat objek socket
+    sock_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Mengatur opsi socket
 
-    sock_server.bind((SERVER_HOST, SERVER_PORT))
+    sock_server.bind((SERVER_HOST, SERVER_PORT))  # Mengikat socket ke alamat IP dan port tertentu
 
-    sock_server.listen()
+    sock_server.listen()  # Mendengarkan koneksi masuk
 
     print("Server ready.....")
 
     while True:
-        sock_client, client_addres = sock_server.accept()
+        sock_client, client_addres = sock_server.accept()  # Menerima koneksi dari client
 
-        request = sock_client.recv(1024).decode()
-        print("Dari client :"+request)
+        request = sock_client.recv(1024).decode()  # Menerima request dari client
+        print("Dari client :" + request)
 
-        response = handle_request(request)
-        sock_client.send(response.encode())
+        response = handle_request(request)  # Menangani request dan menghasilkan response
+        sock_client.send(response.encode())  # Mengirim response ke client
 
-        sock_client.close()
+        sock_client.close()  # Menutup koneksi dengan client
     #endwhile
-    sock_server.close()
+    sock_server.close()  # Menutup socket server
 
 
 def handle_request(request):
     try:
-        filename = request.split()[1][1:]
+        filename = request.split()[1][1:]  # Mendapatkan nama file yang diminta dari request
         with open(filename, 'r') as file:
-            message_body = file.read()
-        response_line = "HTTP/1.1 200 OK\r\n"
-        content_type = "Content-Type: text/html\r\n\r\n"
-        response = response_line+content_type+message_body
+            message_body = "<h1>HTTP/1.1 200 OK</h1>\n\n" + file.read()  # Membaca isi file
+        response_line = "HTTP/1.1 200 OK\r\n"  # Baris pertama pada response
+        print(response_line)
+        content_type = "Content-Type: text/html\r\n\r\n"  # Tipe konten pada response
+        response = response_line + content_type + message_body  # Menggabungkan semua bagian response
     except FileNotFoundError:
-        response_line = "HTTP/1.1 404 Not Found\r\n"
-        content_type = "Content-Type: text/html\r\n\r\n"
-        message_body = "<h1>404 Not Found</h1><p>File tidak tersedia pada server.</p>"
-        response = response_line+content_type+message_body
+        response_line = "HTTP/1.1 404 Not Found\r\n"  # Baris pertama pada response
+        print(response_line)
+        content_type = "Content-Type: text/html\r\n\r\n"  # Tipe konten pada response
+        message_body = "<h1>404 Not Found</h1><p>File tidak tersedia pada server.</p>"  # Isi pesan pada response
+        response = response_line + content_type + message_body  # Menggabungkan semua bagian response
 
     return response
 
 if __name__ == "__main__":
-    tcp_server()
- 
+    tcp_server()  # Memulai server
